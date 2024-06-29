419969175534461185f8759dc82f696351c26df7
inky4832 <inky4832@daum.net>
2024-06-29 14:18:42 +0900

    first commit

--- src/main/java/com/exam/Application.java
+++ src/main/java/com/exam/Application.java
@@ -1,5 +1,6 @@
 package com.exam;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
@@ -9,6 +10,7 @@ import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
+@MapperScan("com.exam.config")
 public class Application  extends SpringBootServletInitializer{
 
 	public static void main(String[] args) {
